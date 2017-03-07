0e3c71c1332ed9b4769f06349924eeecf5d06c63
EricDepagne <eric@depagne.org>
2017-03-07 14:20:38 +0200

    Config file in ini

--- pyts.py
+++ pyts.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# sys imports
+
+# numpy imports
+import numpy as np
+
+# python imports
+from string import Template
+import configparser 
+
+# Parameters
+tbversion = '15.1'
+
+
+def main():
+    print('Starting Turbospectrum version {version}\n'.format(version=tbversion))
+# Loading configuration
+    with open('pyts.ini') as cf:
+        config = configparser.ConfigParser()
+        config.read(cf)
+    print('Configuration : {config}'.format(config=config))
+
+if __name__ == "__main__":
+    main()
